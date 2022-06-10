41c5a9ef93690bb6f80ef245b8fedff577a3f941
이성철 <leesungchul@twinny.co.kr>
2022-06-10 11:01:57 +0900

    DI

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,15 +3,16 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberSerice = new MemberServiceImpl();
-        Member member = new Member(1L, "MemberA", Grade.VIP);
-        memberSerice.join(member);
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
 
-        Member findMember = memberSerice.findMember(1L);
+        Member member = new Member(1L, "MemberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
         System.out.println("new member = " + member.getName());
         System.out.println("find member = " + findMember.getName());
     }
--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,19 +1,29 @@
 package hello.core.member;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import hello.core.AppConfig;
+
 public class MemberServiceTest {
-    MemberService memberService = new MemberServiceImpl();
+
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
-    void join(){
-        //given
+    void join() {
+        // given
         Member member = new Member(1L, "memberA", Grade.VIP);
-        //when
+        // when
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
-        //then
+        // then
         Assertions.assertThat(member).isEqualTo(findMember);
     }
 }
